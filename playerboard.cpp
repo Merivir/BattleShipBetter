6318541fc1a93d983fd513174e51143ece1c52b5
Merivir <118295002+Merivir@users.noreply.github.com>
2024-03-14 02:07:36 +0400

    playerboard.cpp

--- playerboard.cpp
+++ playerboard.cpp
@@ -11,8 +11,7 @@ PlayerBoard::PlayerBoard(QWidget *parent)
 }
 
 void PlayerBoard::setupBoard() {
-    clearBoard(); // Clear any existing ships first
-
+    clearBoard();
     QList<int> shipSizes = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
     QRandomGenerator *rg = QRandomGenerator::global();
 
@@ -24,14 +23,14 @@ void PlayerBoard::setupBoard() {
             Ship::Orientation orientation = rg->bounded(2) == 0 ? Ship::Horizontal : Ship::Vertical;
 
             Ship *newShip = new Ship(size, orientation);
-            newShip->setShipPosition(col, row); // Attempt to set the ship's position
+            newShip->setShipPosition(col, row);
 
             if (canPlaceShip(newShip, col, row) && isSpaceAroundShipFree(newShip)) {
-                ships.append(newShip); // Store the ship
-                scene->addItem(newShip); // Add the ship to the scene
+                ships.append(newShip);
+                scene->addItem(newShip);
                 placed = true;
             } else {
-                delete newShip; // If the ship cannot be placed, delete it and try again
+                delete newShip;
             }
         }
     }
@@ -39,9 +38,9 @@ void PlayerBoard::setupBoard() {
 
 
 void PlayerBoard::drawGrid() {
-    const int gridSize = 10;  // 10x10 grid
-    const int cellSize = 40;  // Each cell is 40x40 pixels
-    const int labelOffset = 20; // Offset for labels from the grid
+    const int gridSize = 10;
+    const int cellSize = 40;
+    const int labelOffset = 20;
 
 
     for (int i = 0; i < gridSize; ++i) {
@@ -49,19 +48,16 @@ void PlayerBoard::drawGrid() {
         columnLabel->setPos(i * cellSize + cellSize / 2, -labelOffset);
     }
 
-      // Draw row labels (1, 2, 3, ...)
     for (int i = 0; i < gridSize; ++i) {
         QGraphicsTextItem *rowLabel = scene->addText(rowLabels[i]);
         rowLabel->setPos(-labelOffset, i * cellSize + cellSize / 2 - rowLabel->boundingRect().height() / 2);
     }
 
-    // Draw the vertical lines
     for (int i = 0; i <= gridSize; ++i) {
         int x = i * cellSize;
         scene->addLine(x, 0, x, gridSize * cellSize, QPen(Qt::black));
     }
 
-    // Draw the horizontal lines
     for (int i = 0; i <= gridSize; ++i) {
         int y = i * cellSize;
         scene->addLine(0, y, gridSize * cellSize, y, QPen(Qt::black));
@@ -70,14 +66,10 @@ void PlayerBoard::drawGrid() {
 }
 
 void PlayerBoard::clearBoard() {
-    // Iterate through the list of ships and delete them
     for (Ship* ship : qAsConst(ships)) {
-        // Remove the ship from the scene
         scene->removeItem(ship);
-        // Delete the ship to free the memory
         delete ship;
     }
 
-    // Clear the list of pointers
     ships.clear();
 }
