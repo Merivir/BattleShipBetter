f8cc56c391775bb8df4fdbcd5d1d0e376fc394a3
Merivir <118295002+Merivir@users.noreply.github.com>
2024-03-14 02:08:08 +0400

    ship.cpp

--- ship.cpp
+++ ship.cpp
@@ -3,25 +3,24 @@
 Ship::Ship(int length, Orientation orientation, QGraphicsItem *parent)
     : QGraphicsRectItem(parent), length(length), orientation(orientation) {
     if (orientation == Horizontal) {
-        setRect(0, 0, length * 40, 40); // Assuming 40x40 grid cells
+        setRect(0, 0, length * 40, 40);
     } else {
         setRect(0, 0, 40, length * 40);
     }
-    setBrush(QBrush(Qt::gray)); // Default color
+    setBrush(QBrush(Qt::gray));
 }
 
 void Ship::setShipPosition(int x, int y) {
-    setPos(x * 40, y * 40); // Convert grid coordinates to pixels
+    setPos(x * 40, y * 40);
 }
 
 QList<QPoint> Ship::getOccupiedCells() const {
     QList<QPoint> cells;
-    QPoint topLeft = this->pos().toPoint(); // Get the top-left point, convert from QPointF to QPoint
-
+    QPoint topLeft = this->pos().toPoint();
     for (int i = 0; i < length; ++i) {
         if (orientation == Horizontal) {
-            cells.append(QPoint(topLeft.x() / 40 + i, topLeft.y() / 40)); // Convert back to grid coordinates
-        } else { // Vertical
+            cells.append(QPoint(topLeft.x() / 40 + i, topLeft.y() / 40));
+        } else {
             cells.append(QPoint(topLeft.x() / 40, topLeft.y() / 40 + i));
         }
     }
